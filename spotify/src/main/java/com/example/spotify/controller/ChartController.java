3cd185c09fb5c0e9a0808cad9969cd25aac374ae
Touhou09 <91013383+Touhou09@users.noreply.github.com>
2023-11-27 21:20:12 +0900

    Playlist api fixed

--- spotify/src/main/java/com/example/spotify/controller/ChartController.java
+++ spotify/src/main/java/com/example/spotify/controller/ChartController.java
@@ -4,12 +4,14 @@ import com.example.spotify.service.ChartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/charts")
 public class ChartController {
     private final ChartService chartService;
 
@@ -18,12 +20,12 @@ public class ChartController {
         this.chartService = chartService;
     }
 
-    @GetMapping("/charts/top-songs/{timeFrame}")
+    @GetMapping("/top-songs/{timeFrame}")
     public List<Map<String, Object>> getTopSongs(@PathVariable String timeFrame) {
         return chartService.displayTopSongs(timeFrame);
     }
 
-    @GetMapping("/charts/top-followed")
+    @GetMapping("/top-followed")
     public List<Map<String, Object>> getTopFollowed() {
         return chartService.displayTopFollowed();
     }
